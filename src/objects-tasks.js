a668297b9e423b7987c1e800796fdc14d6b75ee0
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2024-03-10 23:45:29 +0400

    feat: add solution for isEmptyObject task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -102,8 +102,11 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  if (Object.keys(obj).length === 0) {
+    return true;
+  }
+  return false;
 }
 
 /**
