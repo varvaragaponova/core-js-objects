20dfec7fbb7c06d2a35f3c7ffdef4ef004e02c19
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2024-03-13 00:02:17 +0400

    feat: add solution for task fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -213,8 +213,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const values = Object.values(obj);
+
+  return new proto.constructor(...values);
 }
 
 /**
